018f11515cf59f6a40b4988eeb7fd2add55e2f7c
m7mdaburayan <m7md.aburayan@gmail.com>
2026-09-03 23:36:15 +0300

    Delete src/routes/routes/routes/routes/routes/routes/payments.js

--- src/routes/routes/routes/routes/routes/routes/payments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/paymentsController');
-
-router.post('/', controller.createPayment);
-router.put('/:id/pay', controller.markAsPaid);
-router.get('/contract/:contractId', controller.getPaymentsByContract);
-
-module.exports = router;
