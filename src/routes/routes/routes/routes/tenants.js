64a2eb251d45368fcbd1ba3e2f5df2a401f7a754
m7mdaburayan <m7md.aburayan@gmail.com>
2026-09-03 23:41:32 +0300

    Delete src/routes/routes/routes/routes/tenants.js

--- src/routes/routes/routes/routes/tenants.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/tenantsController');
-
-router.post('/', controller.createTenant);
-router.get('/', controller.getAllTenants);
-
-module.exports = router;
