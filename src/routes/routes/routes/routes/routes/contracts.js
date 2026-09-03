54b9c136c59557850abdabbd9098cb0a6f508866
m7mdaburayan <m7md.aburayan@gmail.com>
2026-09-03 23:39:03 +0300

    Delete src/routes/routes/routes/routes/routes/contracts.js

--- src/routes/routes/routes/routes/routes/contracts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/contractsController');
-
-router.post('/', controller.createContract);
-router.get('/unit/:unitId', controller.getContractsByUnit);
-
-module.exports = router;
