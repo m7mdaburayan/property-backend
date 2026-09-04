604e5ae8db3dfb6c0c238ffa042c4c27c1b5821c
m7mdaburayan <m7md.aburayan@gmail.com>
2026-09-04 14:38:41 +0300

    Update documents.js

--- src/routes/documents.js
+++ src/routes/documents.js
@@ -1,15 +1,8 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
 const controller = require('../controllers/documentsController');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
-});
-const upload = multer({ storage });
-
-router.post('/', upload.single('file'), controller.uploadDocument);
+router.post('/', controller.uploadDocument);
 router.get('/:relatedType/:relatedId', controller.getDocuments);
 
 module.exports = router;
